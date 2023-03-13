4fb1847ce347ddec5a446749c599c8a1fde374d9
FatimaYannel1420 <123432924+FatimaYannel1420@users.noreply.github.com>
2023-03-12 22:37:58 -0600

    EJERCICIO 1

--- I_UNIDAD/I_UNIDAD/Ejercicio1.cs
+++ I_UNIDAD/I_UNIDAD/Ejercicio1.cs
@@ -10,11 +10,31 @@ using System.Windows.Forms;
 
 namespace I_UNIDAD
 {
-    public partial class Form1 : Form
+    public partial class Ejercicio1 : Form
     {
-        public Form1()
+        public Ejercicio1()
         {
             InitializeComponent();
+        }
+
+        private void Ejercicio1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Btn_Sumar_Click(object sender, EventArgs e)
+        {
+            //VARIABLES
+            decimal numero1 = Convert.ToDecimal (Txt_Numero1.Text);
+            decimal numero2 = Convert.ToDecimal(Txt_Numero2.Text);
+
+            decimal resultado = numero1 + numero2;
+            MessageBox.Show("El resultado es:" + resultado);
         }
     }
 }
